43aa1f68d4d31f9c13a9f7a2a626cea6fa705f21
catherine.glossup <catherine.glossop@robotics.utias.utoronto.ca>
2020-08-21 00:35:57 -0400

    starting standalone node

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # triangulation
 
-Stand alone node for triangulation project
+This ROS node is meant to be used with any autonmous car system to locate objects in 3D world coordinates from 2D rosbag images. It's purpose is for adding missing map elements, building maps specific objects in the car's environment, etc. 
+
+The required elements for using this code package are:
+1. A camera feed or rosbag that includes the object(s) of interest
+2. Published Odometry messages
+3. A published transform tree including:
+	a. The camera frame 
+	b. The odometry frame
+	c. Any necessary frames to transform between the previous two frames
+
+The overview of the use of this node is as follows:
+1. A rosbag is chosen or recorded that contains the object(s) of interest. A diverse range of perspectives to take a large number of samples should be taken (30 - 50 images). This diversity will depend on the direction of appraoch to the object and the speed of approach. 
+2. The ROS node is used with the rosbag to collect images and store them in a folder. The images are collected at a regular frequency every second. The ROS node will also record the image location, transformation matrices, and car location in the odometry frame in a CSV file. 
+3. The triangulation python script takes in the information collected from the ROS node and bounding boxes must be manually drawn onto the sample images. 
+4. The script will automatically calculate the 3D world coordinate location of the object of interest. 
+
+Getting Started:
+
+ROS node:
+Once you have collected or choosen the rosbag you will be using for this task, you need to make some changes to the launch file:
+1. 
+
+  
---
+++ triangulation/src/triangulation.cpp
@@ -0,0 +1,54 @@
+// Author: Catherine Glossop
+#include <ros/ros.h>
+#include <ros/console.h>
+#include <ros/package.h>
+#include <fstream>
+#include <triangulation/triangulation_node.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <string>
+#include "utils/geometry_utils.hpp"
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "triangulation");
+    ros::NodeHandle nh;
+    // Get parameters
+    std::string camera_frame, camera_topic,output_append;
+    std::string node_name = ros::this_node::getName();
+    std::string triang_file, folder;
+    nh.getParam(node_name + "/camera_frame",    camera_frame);
+    nh.getParam(node_name + "/camera_topic",    camera_topic);
+    nh.getParam(node_name + "/output_append",   output_append);
+    nh.getParam(node_name + "/triang_file",     triang_file);
+    nh.getParam(node_name + "/folder",          folder);
+    // Subscribers
+    message_filters::Subscriber<sensor_msgs::Image> sub_img(nh, camera_topic, 4);
+    message_filters::Subscriber<nav_msgs::Odometry> sub_odom(nh, "/navsat/odom", 40);
+    // Initialize node object
+    TriangulationNode myNode(nh);
+    myNode.set_node_name();
+    myNode.get_ros_parameters();
+    myNode.initialize_transforms();
+    std::ofstream myFile(triang_file);
+    XmlRpc::XmlRpcValue P;
+    myFile << " Filename; P; Tio; Pose\n";
+    myFile.close();
+    nh.getParam(node_name + "/P", P);
+    Eigen::Matrix4f CAM;
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            double temp = P[4 * i + j];
+            CAM(i, j) = static_cast<float>(temp);
+        }
+    }
+    myNode.set_camera_matrix(CAM);
+    // Synchronize subscribers
+    typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, nav_msgs::Odometry> sync_policy;
+    message_filters::Synchronizer<sync_policy> sync(sync_policy(100), sub_img, sub_odom);
+    sync.registerCallback(boost::bind(&TriangulationNode::callback, myNode, _1, _2));
+
+    ROS_INFO("[OBJ] triangulation running!");
+    ros::spin();
+    return 0;
+}
