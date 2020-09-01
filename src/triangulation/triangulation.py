21a1dc55de3da411f5de04bb988042a8caef3cab
Catherine Glossop <catherine.glossop@robotics.utias.utoronto.ca>
2020-09-01 02:09:19 -0400

    working image collection for python node, no launch errors

--- src/triangulation/triangulation.py
+++ src/triangulation/triangulation.py
@@ -1,47 +1,54 @@
-#!usr/bin/env python
+#! /usr/bin/python
 #Author: Catherine Glossop
 import rospy
-from std_msgs import String
+import std_msgs, nav_msgs
+from nav_msgs.msg import Odometry
 import message_filters
 import csv
 import numpy as np
 from triangulation_node import *
 
-def main(argc, argv):
+def main():
     rospy.init_node('triangulation', anonymous=True)
     rate = rospy.Rate(10) # 10hz
+    global node_name
+    node_name = rospy.get_name()
     #Get parameters
-    rospy.get_param(node_name + "/camera_frame",    camera_frame)
-    rospy.get_param(node_name + "/camera_topic",    camera_topic)
-    rospy.get_param(node_name + "/output_append",   output_append)
-    rospy.get_param(node_name + "/triang_file",     triang_file)
-    rospy.get_param(node_name + "/folder",          folder)
-    rospy.get_param(node_name + "/odom",            odom)
-    rospy.get_param(node_name + "/collect_image",    collect_image)
+    camera_frame = rospy.get_param(node_name + "/camera_frame")
+    camera_topic = rospy.get_param(node_name + "/camera_topic")
+    output_append = rospy.get_param(node_name + "/output_append")
+    triang_file = rospy.get_param(node_name + "/triang_file")
+    folder = rospy.get_param(node_name + "/folder")
+    odom = rospy.get_param(node_name + "/odom")
+    collect_image = rospy.get_param(node_name + "/collect_image")
     # Subscribers
     sub_img = message_filters.Subscriber(camera_topic, sensor_msgs.msg.Image)
-    sub_odom = message_filters.Subscriber(odom, nav_msgs.msg.Odometry)
+    sub_odom = message_filters.Subscriber(odom, Odometry)
     #Initialize node object
     myNode = TriangulationNode()
     myNode.set_node_name()
     myNode.get_ros_parameters()
-    myNode.initialize_transforms()
-    open(triang_file, 'w') as myFile:
+    with open(triang_file, 'w') as myFile:
         headings = "Filename; P; Tio; Pose\n"
         writer = csv.writer(myFile)
-        writer.write_row(headings)
+        writer.writerow(headings)
         myFile.close()
-    rospy.get_param(node_name + "/P", P)
+    global P
+    P = rospy.get_param(node_name + "/P")
     CAM = np.zeros(shape = (4,4))
-    for i in range(0,3):
-        for j in range(0,3):
-            temp = P[4*i*j]
-            CAM(i,j) = temp
+    for i in range(0,4):
+        for j in range(0,4):
+            temp = P[4*i + j]
+            CAM[i,j] = temp
     myNode.set_camera_matrix(CAM)
-    #Synchronize Subscribers
     sync = message_filters.ApproximateTimeSynchronizer([sub_img, sub_odom], 100, 0.1, allow_headerless=True)
     sync.registerCallback(callback)
-
-    ropy.loginfo("[OBJ] Triangulation running")
+    rospy.loginfo("AM I IN HERE????")
+    rospy.loginfo("[OBJ] Triangulation running")
     rospy.spin()
-    return 0
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
--- src/triangulation/triangulation_node.py
+++ src/triangulation/triangulation_node.py
@@ -1,17 +1,21 @@
-#!usr/bin/env python
+#! /usr/bin/python
 import rospy
-import cv_bridge, cv2, time, csv
+import cv2, csv
+from cv_bridge import CvBridge
 import numpy as np
-import stg_msgs, geometry_msgs, nav_msgs, sensor_msgs, tf2_geometry_msgs
+import std_msgs, geometry_msgs, nav_msgs, sensor_msgs, tf2_geometry_msgs
+import tf2_ros, tf
+import os
 
 class transforms:
 
     def get_inverse_tf(self, T):
-        inv = np.zeros(4,4)
+        inv = np.zeros(shape=(4,4))
+
         R = T[0:3,0:3]
-        t = T[2:,0:3]
+        t = T[0:3,3]
         inv[0:3,0:3] = R.transpose()
-        inv[2:,0:3] = np.matmul(-R.transpose(), t)
+        inv[0:3,3] = np.matmul(-R.transpose(), t)
         return inv
 
     def get_odom_tf(self, odom):
@@ -19,13 +23,13 @@ class transforms:
         init_y = 0.0
         init_z = 0.0
         init_pos_set = False
-        self.get_odom_tf(odom, init_x, init_y, init_z, init_pos_set, false, Toi)
-        return True
+        T = self.get_odom_tf_ex(odom, init_x, init_y, init_z, init_pos_set, False)
+        return T
 
-    def get_odom_tf(self, odom, init_x, init_y, init_z, init_pos_set, init_pos_set, zero_odom, Toi):
+    def get_odom_tf_ex(self, odom, init_x, init_y, init_z, init_pos_set, zero_odom):
         Toi = np.zeros(shape=(4,4))
         if zero_odom:
-            if !(init_pos_set):
+            if init_pos_set == False:
                 init_x = odom.pose.pose.position.x
                 init_y = odom.pose.pose.position.y
                 init_z = odom.pose.pose.position.z
@@ -41,12 +45,12 @@ class transforms:
         p = np.matrix([[x],[y],[z]])
         qbar = np.matrix([[qx],[qy],[qz],[qw]])
         R = np.identity(3)
-        if (np.matmul(qbar.transpose(),qbar) > EPS:
+        if np.matmul(qbar.transpose(),qbar) > EPS:
             epsilon = np.matrix([[qx],[qy],[qz]])
             eta = qw
-            epsilon_cross = np.matrix([[0, -epsilon[2], epsilon[1]], [epsilon[2], 0, -epsilon[0]], [-epsilon[1], epsilon[0], 0]]
+            epsilon_cross = np.matrix([[0, -epsilon[2], epsilon[1]], [epsilon[2], 0, -epsilon[0]], [-epsilon[1], epsilon[0], 0]])
             I = np.identity(3)
-            R = (eta**2 - epsilon.transpose() *epsilon) * I + 2*(epsilon*epsilon.transpose()) -2*eta*epsilon_cross
+            R = (eta**2 - np.matmul(epsilon.transpose(), epsilon)[0,0])*I + np.subtract(2*(epsilon*epsilon.transpose()), 2*eta*epsilon_cross)
         Toi[0:3,0:3] = R.transpose()
         Toi[0:3,:3] = p
         Toi[:3,0:3] = np.zeros(shape=(1,3))
@@ -56,12 +60,13 @@ class transforms:
     def convert_to_eigen(self, tf):
         q_ros = tf.transform.rotation
         p_ros = tf.transform.translation
-        p = np.matrix([[p_ros.x] [p_ros.y], [p_ros.z]])
-        epsilon = np.matrix([[q_ros.x] [q_ros.y], [q_ros.z]])
+        T = np.zeros(shape=(4,4))
+        p = np.matrix([[p_ros.x], [p_ros.y], [p_ros.z]])
+        epsilon = np.matrix([[q_ros.x], [q_ros.y], [q_ros.z]])
         eta = q_ros.w
-        epsilon_cross = np.matrix([[0, -epsilon[2], epsilon[1]], [epsilon[2], 0, -epsilon[0]], [-epsilon[1], epsilon[0], 0]]
+        epsilon_cross = np.matrix([[0, -epsilon[2], epsilon[1]], [epsilon[2], 0, -epsilon[0]], [-epsilon[1], epsilon[0], 0]])
         I = np.identity(3)
-        R = (eta**2 - epsilon.transpose() *epsilon) * I + 2*(epsilon*epsilon.transpose()) -2*eta*epsilon_cross
+        R = (eta**2 - np.matmul(epsilon.transpose(), epsilon)[0,0])*I + np.subtract(2*(epsilon*epsilon.transpose()), 2*eta*epsilon_cross)
         p = -1 * R * p
         T[0:3,0:3] = R.transpose()
         T[0:3,:3] = p
@@ -70,69 +75,84 @@ class transforms:
         return T
 
     def get_transform(self, src_frame, tgt_frame):
-        got_tf = false
-        timeout = 0.025
-        tf_temp
+        got_tf = False
+        timeout = 0.25
         tfBuffer = tf2_ros.Buffer()
         listener = tf2_ros.TransformListener(tfBuffer)
-        while(!got_tf and !rospy.is_shutdown()):
+        while (not got_tf and not rospy.is_shutdown()):
             try:
-                tf_temp = tfBuffer.lookupTransform(src_frame, tgt_frame, rospy.Time(0), rospy.Duration(timeout))
+                tf_temp = tfBuffer.lookup_transform(src_frame, tgt_frame, rospy.Time(0), rospy.Duration(timeout))
                 got_tf = True
-            except:
+            except(tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
                 got_tf = False
-                  rospy.logwarn_throttle(1, "Transform from " + src_frame + " to " + tgt_frame + " was not found! Is TF tree and clock being published?")
+                rospy.logwarn_throttle(1, "Transform from " + src_frame + " to " + tgt_frame + " was not found! Is TF tree and clock being published?")
             if got_tf:
                 T = self.convert_to_eigen(tf_temp)
-        return T
+            else:
+                T = np.zeros(shape=(4,4))
+        return True
 
 class TriangulationNode:
-    def set_camera_matrix(self, P_):
-        P_ = P
+    def set_camera_matrix(self, CAM):
+        P_ = CAM
         return True
 
     def set_node_name(self):
+        global node_name
         node_name = rospy.get_name()
         return True
 
     def initialize_transforms(self):
         tfBuffer = tf2_ros.Buffer()
         tfListener = tf.TransformListener()
-        Tvi = transforms.get_transform()
+        global Tvi
+        x = transforms()
+        Tvi = x.get_transform("odom", "mono_link")
+        return True
+
+    def get_ros_parameters(self):
+        global camera_frame
+        global image_rate
+        global triang_file
+        global folder
+        global collect_image
+        camera_frame = rospy.get_param(node_name + "/camera_frame")
+        image_rate = rospy.get_param(node_name + "/image_rate")
+        triang_file = rospy.get_param(node_name + "/triang_file")
+        folder = rospy.get_param(node_name + "/folder")
+        collect_image = rospy.get_param(node_name + "/collect_image")
         return True
 
 def callback(img, odom):
     bridge = CvBridge()
-    start = time.monotonic()
+    start = rospy.rostime.Time.now()
+    node_name = rospy.get_name()
     img_save = bridge.imgmsg_to_cv2(img, desired_encoding="passthrough")
-    data = open(os.path.join(directory,triang_file))
-    mycsv = csv.writer(data, 'a')
-    print("IN PYTHON NODE")
+    data = open(triang_file, 'a')
+    mycsv = csv.writer(data)
+    rospy.loginfo("IN PYTHON NODE")
     if collect_image == 1:
         rospy.Rate(image_rate)
-        filetime = rospy.rostime.Time.now.secs()
+        filetime = rospy.rostime.Time.now()
         filename = folder + str(filetime) + ".png"
-        cv2.imwrite(filename, filename)
-        Toi = transforms.get_odom_tf(odom)
-        T_ic = transforms.get_transform(tfBuffer, camera_frame, imu_frame)
+        cv2.imwrite(filename, img_save)
+        transform = transforms()
+        Toi = transform.get_odom_tf(odom)
+        Tio = transform.get_inverse_tf(Toi)
+        Tic = transform.get_transform(camera_frame, "imu_link")
         E = np.zeros(shape =(4,4))
-        E = transforms.get_inverse_tf(Toi * Tic)
-        P_tilda = np.reshape(np.matmul(P_d * E), (-1,16))
+        E = transform.get_inverse_tf(Toi * Tic)
+        P = rospy.get_param(node_name + "/P")
+        P = np.matrix(P)
+        P = P.reshape(4,4)
+        P_tilda = np.matmul(P, E)
+        P_tilda = P_tilda.reshape(-1,16)
         Tio = np.reshape(Tio, (-1,16))
         Pose = np.matrix([odom.pose.pose.position.x, odom.pose.pose.position.y, odom.pose.pose.position.z])
         row = [filename, P_tilda, Tio, Pose]
         mycsv.writerow(row)
-        mycsv.close()
-    print
-    stop = time.monotonic()
+        data.close()
+    stop = rospy.rostime.Time.now()
     elapsed = stop - start
-    rospy.logdebug("[OBJ] TRIANGULATION TIME: " + str(elasped))
-    return True
-
-def get_ros_parameters():
-    rospy.get_param(node_name + "/camera_frame",        camera_frame)
-    rospy.get_param(node_name + "/image_rate",          image_rate)
-    rospy.get_param(node_name + "/triang_file",         triang_file)
-    rospy.get_param(node_name + "/folder",              folder)
-    rospy.get_param(node_name + "/collect_image",       collect_image)
+    rospy.logdebug("[OBJ] TRIANGULATION TIME: " + str(elapsed))
     return True
